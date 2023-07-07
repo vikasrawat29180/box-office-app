00c7296204e1d0d43fb9387befad7fe457643ba6
vikasrawat29180 <vikasrawat29180@gmail.com>
2023-07-07 14:12:27 +0530

    added react-router-dom

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div>
+      <Link to="/starred">Go to Starred page</Link>
+    </div>
+  );
+};
+export default Home;
